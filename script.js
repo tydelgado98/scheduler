ac2e3a7a64a3007a3544d3eade111f004583b449
Justin Delgado <delgadojustin100@yahoo.com>
2023-09-06 06:58:52 -0400

    deleting comments

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+
 );
 
 let saveBtn = $('.saveBtn');
